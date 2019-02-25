e38ca3cd18ffaa4356d00abce07d4d7248e75a54
Punit Jajodia <pnt.jjd@gmail.com>
2019-02-26 00:10:24 +0545

    Updates

---
+++ precache-manifest.affe3ec87fb5ccf11a993334ce3d53ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6af3151fea2eee701e7",
+    "url": "/nirmaan/static/css/main.f6c2f0c9.chunk.css"
+  },
+  {
+    "revision": "e6af3151fea2eee701e7",
+    "url": "/nirmaan/static/js/main.e6af3151.chunk.js"
+  },
+  {
+    "revision": "b38b4f674b1a125e6cd7",
+    "url": "/nirmaan/static/js/runtime~main.b38b4f67.js"
+  },
+  {
+    "revision": "685717e2cef83bbc9762",
+    "url": "/nirmaan/static/js/2.685717e2.chunk.js"
+  },
+  {
+    "revision": "df373ae0dbd055047f76dc0283b293ea",
+    "url": "/nirmaan/index.html"
+  }
+];
