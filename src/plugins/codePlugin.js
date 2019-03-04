bd1615b93a4e038c4cb79b9814435e392e87b19d
Punit Jajodia <pnt.jjd@gmail.com>
2019-03-04 12:39:13 +0545

    Added code paste feature

--- src/plugins/codePlugin.js
+++ src/plugins/codePlugin.js
@@ -3,28 +3,49 @@ import { MarkHotKey } from "../marks/HotKeys";
 import { CodeNode } from "../nodes/CodeNode";
 import { NonExecutableCodeNode } from "../nodes/NonExecutableCodeNode";
 import { SampleOutput } from "../nodes/SampleOutput";
+import { getEventTransfer } from "slate-react";
 
 export const codeMarkPlugin = MarkHotKey({
   type: "code",
   key: "`"
 });
 
+export const isCodeBlock = block => {
+  return (
+    block.type === "code" ||
+    block.type === "nonexecutable-code" ||
+    block.type === "sample-output"
+  );
+};
+
 export const codeNodePlugin = options => {
   return {
+    queries: {
+      isCode: editor => {
+        return editor.value.blocks.some(block => isCodeBlock(block));
+      }
+    },
     onKeyDown: (event, editor, next) => {
       if (!event.ctrlKey) return next();
 
       switch (event.key) {
         case "`":
           event.preventDefault();
-          const isCode = editor.value.blocks.some(
-            block => block.type === "code"
-          );
+          const isCode = editor.isCode();
           editor.setBlocks(isCode ? "paragraph" : "code");
           break;
         default:
           return next();
       }
+    },
+    onPaste: (event, editor, next) => {
+      const { text } = getEventTransfer(event);
+      const isCode = editor.isCode();
+
+      if (!isCode) {
+        return next();
+      }
+      editor.insertText(text);
     },
     renderNode: (props, editor, next) => {
       switch (props.node.type) {
