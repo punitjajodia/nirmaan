aea74b91b414cf5547b0cc314703b4325aeaad70
Punit Jajodia <pnt.jjd@gmail.com>
2019-04-05 14:47:48 +0545

    Popup should have more z-index than anything else

---
+++ src/plugins/linkPlugin.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export const linkPlugin = options => {
+  return {
+    commands: {
+      wrapLink: (editor, href) => {
+        editor.wrapInline({
+          type: "link",
+          data: { href }
+        });
+        editor.moveToEnd();
+      },
+      unwrapLink: editor => {
+        editor.unwrapInline("link");
+      }
+    },
+    queries: {
+      hasLinks: editor => {
+        const { value } = editor;
+        return value.inlines.some(inline => inline.type === "link");
+      }
+    },
+    renderNode: (props, editor, next) => {
+      const { attributes, node, children } = props;
+      switch (node.type) {
+        case "link":
+          const { data } = node;
+          const href = data.get("href");
+          return (
+            <a {...attributes} href={href}>
+              {children}
+            </a>
+          );
+        default:
+          return next();
+      }
+    }
+  };
+};
---
+++ src/toolbars/Popup.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const PopupWrapper = styled.div`
+  width: 50%;
+  padding: 30px 50px;
+  z-index: 100000000;
+`;
