9ae25209d9a4e6cc941cd0629e5410d64bb8753f
Punit Jajodia <punitjajodia@52.19.59.227>
2020-11-09 14:02:11 +0545

    Updates

---
+++ precache-manifest.b6d90db1a9550887ca535564dee3d2dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049177746ec080fd3dbcc58993e14c5e",
+    "url": "/nirmaan/index.html"
+  },
+  {
+    "revision": "7e9ec47887ec0d933af5",
+    "url": "/nirmaan/static/css/main.fd860a05.chunk.css"
+  },
+  {
+    "revision": "42cab1674f8948750972",
+    "url": "/nirmaan/static/js/2.83f1cc74.chunk.js"
+  },
+  {
+    "revision": "994c3ffc0c05336b82446ace49fee855",
+    "url": "/nirmaan/static/js/2.83f1cc74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e9ec47887ec0d933af5",
+    "url": "/nirmaan/static/js/main.c894f869.chunk.js"
+  },
+  {
+    "revision": "fcaee3129991d87f67f8",
+    "url": "/nirmaan/static/js/runtime-main.e4d0c25c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nirmaan/precache-manifest.4ac900d8f86f7ec3a04ec28ac1ff6c3b.js"
+  "/nirmaan/precache-manifest.b6d90db1a9550887ca535564dee3d2dc.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nirmaan/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nirmaan/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
